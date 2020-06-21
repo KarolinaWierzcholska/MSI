10a5a3c3672db634683ad56d593c6364dd10d1e6
countwaldstein <64553055+countwaldstein@users.noreply.github.com>
2020-06-21 21:18:09 +0200

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,21 @@
+from .SEA import SEA
+from .AWE import AWE
+from .AUE import AUE
+from .WAE import WAE
+from .OnlineBagging import OnlineBagging
+from .OOB import OOB
+from .UOB import UOB
+from .DWM import DWM
+from .AUE1 import AUE1
+
+__all__ = [
+    "OnlineBagging",
+    "OOB",
+    "SEA",
+    "UOB",
+    "WAE",
+    "AWE",
+    "AUE",
+    "DWM",
+    "AUE1"
+]
